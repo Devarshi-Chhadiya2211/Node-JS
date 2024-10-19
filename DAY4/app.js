4af6dd627cc6bb365d5edade933c27c098e645f2
Devarshi-Chhadiya2211 <devarshichhadiya@gmail.com>
2024-10-19 15:22:37 +0530

    unlink-fs

---
+++ DAY4/app.js
@@ -0,0 +1,11 @@
+// In Node.js, unlink() is a method provided by the fs (file system) module 
+// that allows you to delete a file from the file system.
+
+const fs = require('fs');
+
+try {
+  fs.unlinkSync('unlink.txt');
+  console.log('File successfully deleted');
+} catch (err) {
+  console.error('Error deleting the file:', err);
+}
