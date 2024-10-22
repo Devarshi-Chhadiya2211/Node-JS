7e64a69583ee6b56072ee566c0d30f1a91663f8c
Devarshi-Chhadiya2211 <devarshichhadiya@gmail.com>
2024-10-22 11:06:12 +0530

    node

---
+++ DAY6/app.js
@@ -0,0 +1,12 @@
+// modules
+   //--> Local module
+
+   console.log("hello world");
+
+   const {add,sub,mul,div} = require("./sums.js")
+
+
+   console.log(add(4,2))
+   console.log(sub(4,2))
+   console.log(mul(4,2))
+   console.log(div(4,2))
