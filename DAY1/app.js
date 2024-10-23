2344d3305acd1dcadd558d8d47a326c49466a37b
Devarshi-Chhadiya2211 <devarshichhadiya@gmail.com>
2024-10-23 16:51:24 +0530

    local module

--- DAY1/app.js
+++ DAY1/app.js
@@ -6,7 +6,7 @@
    const {add,sub,mul,div} = require("./math.js")
 
 
-   console.log(add(4,2))
-   console.log(sub(4,2))
-   console.log(mul(4,2))
-   console.log(div(4,2))
+   console.log(add(6,2))
+   console.log(sub(6,2))
+   console.log(mul(6,2))
+   console.log(div(6,2))
