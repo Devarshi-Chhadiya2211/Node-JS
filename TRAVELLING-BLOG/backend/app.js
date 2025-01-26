2b96831a0d6999d5481051b4ac85eb775987f2ee
Devarshi-Chhadiya2211 <devarshichhadiya@gmail.com>
2025-01-26 19:11:03 +0530

    travelling-blog

--- TRAVELLING-BLOG/backend/app.js
+++ TRAVELLING-BLOG/backend/app.js
@@ -1,18 +1,27 @@
-const express=require('express')
-const UserRouter = require('./Routes/UserRouter')
-const ProductRouter = require('./Routes/ProductRouter')
-const cors=require('cors')
-const connection = require('./Config/db')
-const app=express()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+const userRoutes = require("./routes/userRoutes")
+const bookingRoutes = require("./routes/bookingRoutes")
+
+const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
 
-// app.use('/user',UserRouter)
-// app.use('/',ProductRouter)
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err))
 
-app.listen(6276,()=>{
-    connection()
-    console.log('server running at 6276')
-})
+app.use("/", userRoutes)
+app.use("/", bookingRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
